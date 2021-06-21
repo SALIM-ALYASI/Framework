6309b721d69babe5c38d3b660f1a9eed7334136a
الياسي والأمل <r.m.alyasi@gmail.com>
2021-06-21 23:30:44 +0400

    6

---
+++ Framework/Framework.swift
@@ -0,0 +1,13 @@
+//
+//  Framework.swift
+//  Framework
+//
+//  Created by الياسي on 21/06/2021.
+//
+import Foundation
+public class SALIM {
+   private init() {}
+   public static func SALIMALYASI() -> String {
+       return "سالم صح "
+   }
+}
